7a60756e9408a52ea24df0d26c86c5b26aa9f30a
Hisam Fahri <hisam@alphaaiauto.com>
2022-01-25 22:42:21 +0700

    feat(commit): add --all flag

--- cmd/commit.go
+++ cmd/commit.go
@@ -10,123 +10,144 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var All bool
+
 type commitStruct struct {
 	Name        string
 	Description string
 }
 
-func Commit() *cobra.Command {
-	commitTypes := []commitStruct{
-		{Name: "feat", Description: "A new feature"},
-		{Name: "fix", Description: "A bug fix"},
-		{Name: "docs", Description: "Documentation only changes"},
-		{Name: "style", Description: "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"},
-		{Name: "refactor", Description: "A code change that neither fixes a bug nor adds a feature"},
-		{Name: "perf", Description: "A code change that improves performance"},
-		{Name: "test", Description: "Adding missing or correcting existing tests"},
-		{Name: "chore", Description: "Changes to the build process or auxiliary tools and libraries such as documentation generation"},
-	}
+var commitTypes = []commitStruct{
+	{Name: "feat", Description: "A new feature"},
+	{Name: "fix", Description: "A bug fix"},
+	{Name: "docs", Description: "Documentation only changes"},
+	{Name: "style", Description: "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"},
+	{Name: "refactor", Description: "A code change that neither fixes a bug nor adds a feature"},
+	{Name: "perf", Description: "A code change that improves performance"},
+	{Name: "test", Description: "Adding missing or correcting existing tests"},
+	{Name: "chore", Description: "Changes to the build process or auxiliary tools and libraries such as documentation generation"},
+}
 
-	templates := &promptui.SelectTemplates{
-		Label:    "{{ . }}?",
-		Active:   "> {{ .Name | cyan }}: {{ .Description | red }}",
-		Inactive: "  {{ .Name | cyan }}: {{ .Description | red }}",
-		Selected: "> {{ .Name | cyan }}: {{ .Description | red }}",
-		// 		Details: `
-		// --------- Change Type ----------
-		// {{ "Name:" | faint }}	{{ .Name }}
-		// {{ "Desc:" | faint }}	{{ .Description }}`,
-	}
+var templates = &promptui.SelectTemplates{
+	Label:    "{{ . }}?",
+	Active:   "> {{ .Name | cyan }}: {{ .Description | red }}",
+	Inactive: "  {{ .Name | cyan }}: {{ .Description | red }}",
+	Selected: "> {{ .Name | cyan }}: {{ .Description | red }}",
+	// 		Details: `
+	// --------- Change Type ----------
+	// {{ "Name:" | faint }}	{{ .Name }}
+	// {{ "Desc:" | faint }}	{{ .Description }}`,
+}
 
-	return &cobra.Command{
-		Use:   "commit",
-		Short: "Commit is CLI to help you structurized your commit message",
-		Long: `Simple but powerful CLI to help your commit message to follow
+var commitCommand = &cobra.Command{
+	Use:   "commit",
+	Short: "Commit is CLI to help you structurized your commit message",
+	Long: `Simple but powerful CLI to help your commit message to follow
 		conventional commit message`,
-		Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, args []string) {
 
-			changeTypePrompt := promptui.Select{
-				Label:     "Type of Change",
-				Items:     commitTypes,
-				Templates: templates,
-				Size:      8,
+		allFlag, _ := cmd.Flags().GetBool("all")
+
+		changeTypePrompt := promptui.Select{
+			Label:     "Type of Change",
+			Items:     commitTypes,
+			Templates: templates,
+			Size:      8,
+		}
+
+		// Scope
+
+		validateScope := func(input string) error {
+			if len(input) > 25 {
+				return errors.New("commit scope must have less than 25 characters")
+			} else if len(input) < 2 {
+				return errors.New("commit scope must have more than 2 characters")
 			}
+			return nil
+		}
 
-			// Scope
+		scopePrompt := promptui.Prompt{
+			Label:    "Scope of changes (eg. file, function, etc)",
+			Validate: validateScope,
+		}
 
-			validateScope := func(input string) error {
-				if len(input) > 25 {
-					return errors.New("commit scope must have less than 25 characters")
-				} else if len(input) < 2 {
-					return errors.New("commit scope must have more than 2 characters")
-				}
-				return nil
+		// message
+
+		validateCommitMessage := func(input string) error {
+			if len(input) > 100 {
+				return errors.New("commit message must have less than 100 characters")
+			} else if len(input) < 5 {
+				return errors.New("commit message must have more than 5 characters")
 			}
+			return nil
+		}
 
-			scopePrompt := promptui.Prompt{
-				Label:    "Scope of changes (eg. file, function, etc)",
-				Validate: validateScope,
-			}
+		commitMessagePrompt := promptui.Prompt{
+			Label:    "Commit message title (min 5 & max 100)",
+			Validate: validateCommitMessage,
+		}
 
-			// message
+		i, _, errType := changeTypePrompt.Run()
 
-			validateCommitMessage := func(input string) error {
-				if len(input) > 100 {
-					return errors.New("commit message must have less than 100 characters")
-				} else if len(input) < 5 {
-					return errors.New("commit message must have more than 5 characters")
-				}
-				return nil
-			}
+		if errType != nil {
+			fmt.Printf("Commit type is required: %v\n", errType)
+			return
+		}
 
-			commitMessagePrompt := promptui.Prompt{
-				Label:    "Commit message title (min 5 & max 100)",
-				Validate: validateCommitMessage,
-			}
+		commitScope, errScope := scopePrompt.Run()
 
-			i, _, errType := changeTypePrompt.Run()
+		if errScope != nil {
+			fmt.Printf("Commit failed %v\n", errScope)
+			return
+		}
 
-			if errType != nil {
-				fmt.Printf("Commit type is required: %v\n", errType)
-				return
-			}
+		commitMessage, errMessage := commitMessagePrompt.Run()
 
-			commitScope, errScope := scopePrompt.Run()
+		if errMessage != nil {
+			fmt.Printf("Commit failed %v\n", errMessage)
+			return
+		}
 
-			if errScope != nil {
-				fmt.Printf("Commit failed %v\n", errScope)
-				return
-			}
+		fullCommit := "git commit -m \"" + commitTypes[i].Name + "(" + commitScope + "): " + commitMessage + "\""
 
-			commitMessage, errMessage := commitMessagePrompt.Run()
-
-			if errMessage != nil {
-				fmt.Printf("Commit failed %v\n", errMessage)
-				return
-			}
-
-			fullCommit := "git commit -m \"" + commitTypes[i].Name + "(" + commitScope + "): " + commitMessage + "\""
-
-			for _, c := range []string{fullCommit} {
+		if allFlag {
+			for _, c := range []string{"git add ."} {
 				p := script.Exec(c)
-				fmt.Println("Exit Status:", p.ExitStatus())
+				// fmt.Println("Exit Status:", p.ExitStatus())
 				if err := p.Error(); err != nil {
 					p.SetError(nil)
 					out, _ := p.Stdout()
 					fmt.Println(out)
 				} else {
 					out, _ := p.Stdout()
-					fmt.Println(out)
+					fmt.Println("Successfully run `git add .`\nExit status: ", out)
 				}
-				fmt.Println("---")
 			}
-		},
-	}
+		}
+
+		for _, c := range []string{fullCommit} {
+			p := script.Exec(c)
+			fmt.Println("Exit Status:", p.ExitStatus())
+			if err := p.Error(); err != nil {
+				p.SetError(nil)
+				out, _ := p.Stdout()
+				fmt.Println(out)
+			} else {
+				out, _ := p.Stdout()
+				fmt.Println(out)
+			}
+			fmt.Println("---")
+		}
+	},
 }
 
 func Execute() {
-	if err := Commit().Execute(); err != nil {
+	if err := commitCommand.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	commitCommand.PersistentFlags().BoolVarP(&All, "all", "a", false, "Commit all changes in current directory")
 }
--- go.mod
+++ go.mod
@@ -2,14 +2,16 @@ module github.com/hisamafahri/commit
 
 go 1.17
 
-require github.com/spf13/cobra v1.3.0
+require (
+	github.com/bitfield/script v0.18.3
+	github.com/manifoldco/promptui v0.9.0
+	github.com/spf13/cobra v1.3.0
+)
 
 require (
 	bitbucket.org/creachadair/shell v0.0.6 // indirect
-	github.com/bitfield/script v0.18.3 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/manifoldco/promptui v0.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
 )
