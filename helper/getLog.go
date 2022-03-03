f9755c001fb4d76ec562a8925f98289a1af3a04c
Hisam Fahri <me@hisamafahri.com>
2022-03-03 18:27:34 +0700

    feat(log): add ref names

--- helper/getLog.go
+++ helper/getLog.go
@@ -9,7 +9,7 @@ import (
 func GetLog() (string, error) {
 	var output string
 
-	command := "git log --pretty=\"format:\x1B[38;2;249;38;114m%h\x1B[0m %s \u001b[38;5;242m %cn - %cr\u001b[0m  \""
+	command := "git log --pretty=\"format:\x1B[38;2;249;38;114m%h\x1B[0m \u001b[33m%d\u001b[0m %s \u001b[38;5;242m %cn - %cr\u001b[0m  \""
 	for _, c := range []string{command} {
 		p := script.Exec(c)
 		if err := p.Error(); err != nil {
